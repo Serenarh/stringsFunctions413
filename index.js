6d8d2cdc124720c930197e8b224c89aa2cd00c76
Serena Hasler <srhasler@yahoo.com>
2020-04-11 12:05:10 -0500

    Add comments

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const none = "Smith";
 
 function getTraditionalGender(nameWithSalutation) {
   if (nameWithSalutation.startsWith("Mr.")) {
-    return "M";
+    return "M"; //will return M if name passed begins with "Mr."
   }
 
   if (nameWithSalutation.startsWith("Mrs.")) {
-    return "F";
+    return "F"; //will return F if name passed begins with "Mrs."
   }
 
   if (nameWithSalutation.startsWith("Ms.")) {
-    return "F";
+    return "F"; //will return F if name passed begins with "Ms."
   }
 
-  return "Unknown";
+  return "Unknown"; //will return Unknown if name passed does not begin with "Mr." "Mrs." or "Ms."
 }
 
-console.log(getTraditionalGender(woman2));
-console.log(getTraditionalGender(none));
-console.log(getTraditionalGender(woman1));
-console.log(getTraditionalGender(man));
+console.log(getTraditionalGender(woman2)); //returns F
+console.log(getTraditionalGender(none)); //returns Unknown
+console.log(getTraditionalGender(woman1)); //returns F
+console.log(getTraditionalGender(man)); //returns M
