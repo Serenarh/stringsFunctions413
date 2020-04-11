2354bb7c00e915f0fa817340bf17d424e063c3db
Serena Hasler <srhasler@yahoo.com>
2020-04-11 11:20:25 -0500

    Add example concatenation

---
+++ index.js
@@ -0,0 +1,5 @@
+//TODO Peruse the docs regarding String.prototype - specifically the methods. YK the drill - pick 5 and make up your own examples with comments to show me that you understand how to apply said methods. 5 points.
+
+const good = "Good";
+const morning = "Morning!";
+console.log(good.concat(" ", morning)); //concatenates first string good with second string morning to print "Good Morning!";
