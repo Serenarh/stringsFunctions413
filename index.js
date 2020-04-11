53c65a60d2505083f44e9924262d2394cacda8a9
Serena Hasler <srhasler@yahoo.com>
2020-04-11 11:36:30 -0500

    Add example lastIndexOf

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ console.log(good.concat(" ", morning)); //concatenates first string good with se
 
 const spring = "Spring began on March 19";
 console.log(spring.endsWith("March 19")); //checks to see if string "spring" ends with "March 19"; if so, returns "true"; if not, returns "false"; here returns "true"
+
+const color = "Blue is my favorite color. My favorite color is blue.";
+console.log(color.lastIndexOf("Blue")); //will find last index of Blue--returns 0 since lastIndexOf is case sensitive
+console.log(color.lastIndexOf("blue")); //will find last index of blue; will return 48 as this "blue" begins with lc "b"
