1b82d3fc6dcd8b471f4d5c3436be20bae05eec4d
Serena Hasler <srhasler@yahoo.com>
2020-04-11 11:57:39 -0500

    Add constants for traditional gender detector

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-//TODO Peruse the docs regarding String.prototype - specifically the methods. YK the drill - pick 5 and make up your own examples with comments to show me that you understand how to apply said methods. 5 points.
+//Create a 'traditional' gender detector. - 5 pts.
+//TODO: Given a STRING such as 'Mr. Misra', return back 'M.' //Given a STRING that STARTS WITH 'Mrs.' or 'Ms.', return back 'F.' //Else, RETURN 'Unknown'
 
-const good = "Good";
-const morning = "Morning!";
-console.log(good.concat(" ", morning)); //concatenates first string good with second string morning to print "Good Morning!";
-
-const spring = "Spring began on March 19";
-console.log(spring.endsWith("March 19")); //checks to see if string "spring" ends with "March 19"; if so, returns "true"; if not, returns "false"; here returns "true"
-
-const color = "Blue is my favorite color. My favorite color is blue.";
-console.log(color.lastIndexOf("Blue")); //will find last index of Blue--returns 0 since lastIndexOf is case sensitive
-console.log(color.lastIndexOf("blue")); //will find last index of blue; will return 48 as this "blue" begins with lc "b"
-
-const yay = "Yay! ";
-console.log(yay.repeat(10)); //repeat causes indicated string to be repeated the indicated # of times; here "Yay! " will be repeated 10x
-
-const word = "HUSH!";
-console.log(word.toLowerCase()); //change indicated string (here represented by variable word) to lower case
+const man = "Mr. Smith";
+const woman1 = "Mrs. Smith";
+const woman2 = "Ms. Smith";
+const none = "Smith";
